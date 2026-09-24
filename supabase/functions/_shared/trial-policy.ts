de7e49fbc023bf0a46afbf1dcad2294a53995a91
jefersond <jeferson.zanotell@gmail.com>
2026-09-24 20:32:12 -0300

    Remove obsolete global four-day trial source

--- supabase/functions/_shared/trial-policy.ts
+++
@@ -1,7 +0,0 @@
-export const TRIAL_DURATION_DAYS = 4;
-export const TRIAL_POLICY_VERSION = "4d_2026_09";
-export const TRIAL_REQUIRES_CARD = true;
-
-export function trialTypeTag() {
-  return `${TRIAL_DURATION_DAYS}_day_card_required`;
-}
